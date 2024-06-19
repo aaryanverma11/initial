295547454a804f45f032fc406b4a3caf3f21e461
aaryanverma11 <aaryanverma2806@gmail.com>
2024-06-19 19:22:57 +0530

    1.infinity_war.py  2.program.py  3.practice2.0.py

---
+++ program2.py
@@ -0,0 +1,11 @@
+# str ="apna ghar"
+# print(str[:4]) #[0:4]
+# print(str[5:]) #[5:len(str)]
+
+
+# str = "apple"
+# print(str[-5:-2])
+
+
+str ="i am from studying python from my home"
+print(str.count("o"))
